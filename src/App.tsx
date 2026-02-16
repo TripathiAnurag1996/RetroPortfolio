0510f1ee44ed7361fa0f4c28633c54118fa19a2a
Anurag <anurag.akt@gmail.com>
2026-02-16 23:10:52 +0530

    fix(seo): remove nested HelmetProvider to restore head injection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { HelmetProvider } from 'react-helmet-async'
 import { WindowProvider } from './context/WindowContext'
 import { ThemeProvider } from './context/ThemeContext'
 import Desktop from './components/Desktop/Desktop'
@@ -7,15 +6,13 @@ import CookieConsent from './components/Common/CookieConsent'
 
 function App() {
   return (
-    <HelmetProvider>
-      <ThemeProvider>
-        <WindowProvider>
-          <SEO />
-          <Desktop />
-          <CookieConsent />
-        </WindowProvider>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ThemeProvider>
+      <WindowProvider>
+        <SEO />
+        <Desktop />
+        <CookieConsent />
+      </WindowProvider>
+    </ThemeProvider>
   )
 }
 
